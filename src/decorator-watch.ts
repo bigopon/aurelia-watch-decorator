13c877e21a1a003ce8fcb21c9af5e44ba967be0e
Binh Vo <binh.vo@open.edu.au>
2020-09-08 20:09:55 +1000

    feat(watch): initial implementation

---
+++ src/decorator-watch.ts
@@ -0,0 +1,48 @@
+import { metadata } from "aurelia-metadata";
+import { HtmlBehaviorResource } from "aurelia-templating";
+import { Constructable, IPropertyAccessFn, IWatcherCallback } from "./interfaces";
+import { patchController } from ".";
+
+export function watch<T extends object = object>(
+  expressionOrPropertyAccessFn: string | IPropertyAccessFn<T>,
+  changeHandlerOrCallback?: string | IWatcherCallback<T>,
+): any {
+  patchController();
+
+  return function decorator(
+    target: Constructable<T> | Constructable<T>['prototype'],
+    key?: PropertyKey,
+    descriptor?: PropertyDescriptor,
+  ) {
+    // class decorator?
+    const isClassDecorator = key == null;
+    const ctor = isClassDecorator ? target : target.constructor;
+
+    // basic validation
+    if (typeof changeHandlerOrCallback === 'string' && !(changeHandlerOrCallback in ctor.prototype)) {
+      throw new Error(`Invalid change handler config. Method not found in class ${ctor.name}`);
+    }
+
+    if (!isClassDecorator && typeof descriptor.value !== 'function') {
+      throw new Error(`decorated target ${String(key)} is not a class method.`);
+    }
+
+    const behaviorMetadata = metadata.getOrCreateOwn(
+      metadata.resource,
+      HtmlBehaviorResource,
+      ctor as Constructable<T>,
+    ) as HtmlBehaviorResource;
+
+    const watchExpressions = behaviorMetadata.$watch = (behaviorMetadata.$watch || [])
+    watchExpressions.push({
+      expression: expressionOrPropertyAccessFn,
+      callback: isClassDecorator ? changeHandlerOrCallback : descriptor.value,
+    });
+
+    if (isClassDecorator) {
+      return;
+    }
+
+    return descriptor;
+  }
+}
---
+++ src/scope-expression-observer.ts
@@ -0,0 +1,68 @@
+import {
+  Binding,
+  connectable as $connectable,
+  Expression,
+  Scope,
+  ObserverLocator,
+} from 'aurelia-binding';
+import {
+  IWatcherCallback,
+  IScopeExpressionObserver
+} from './interfaces';
+
+const connectable = $connectable as any;
+
+/**
+ * @internal The interface describes methods added by `connectable` & `subscriberCollection` decorators
+ */
+export interface ScopeExpressionObserver extends Binding {
+  _version: number;
+  unobserve(all?: boolean): void;
+}
+
+export class ScopeExpressionObserver<T extends object = object> implements IScopeExpressionObserver {
+  /**@internal */
+  private oldValue: any;
+
+  constructor(
+    public scope: Scope,
+    public expression: Expression,
+    public callback: IWatcherCallback<T>,
+    /**@internal */
+    private lookupFunctions: any,
+    /**@internal used by expression.connect */
+    public observerLocator: ObserverLocator,
+  ) {
+    this.scope = scope;
+    this.lookupFunctions = lookupFunctions;
+  }
+
+  /**@internal */
+  call() {
+    let scope = this.scope;
+    let expression = this.expression;
+    let newValue = expression.evaluate(scope, this.lookupFunctions);
+    let oldValue = this.oldValue;
+    if (!Object.is(newValue, oldValue)) {
+      this.oldValue = newValue;
+      const obj = scope.bindingContext;
+      this.callback.call(obj, newValue, oldValue, obj);
+    }
+    this._version++;
+    expression.connect(this, scope);
+    this.unobserve(false);
+  }
+
+  begin() {
+    this.oldValue = this.expression.evaluate(this.scope, this.lookupFunctions);
+    this.expression.connect(this, this.scope);
+  }
+
+  end() {
+    this.unobserve(true);
+    this.oldValue = void 0;
+  }
+}
+
+// use this instead of decorator to avoid extra generated code
+connectable()(ScopeExpressionObserver);
